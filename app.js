497c3f2f38fb29356fb2df27eca95f3215382ab6
Gekuro <piokmiec@gmail.com>
2022-12-11 18:58:10 +0100

    simple login page styling

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 const app = express();
 
-const port = 8000;
+const port = 80;
+app.use(express.static('static'));
+app.use(favicon(path.join(__dirname,'static/favicon.ico')));
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.get('/', function(req, res) {
+app.get('/', (_, res)=>{
     res.sendFile(path.join(__dirname, '/views/login.html'));
 });
 
-app.use(express.static('static'));
+app.post('/log', (req, res)=>{
+    console.log(req.body);
+    res.end();
+});
 
 app.listen(port, ()=>{
     console.log(`Server running on port: ${port}`);
